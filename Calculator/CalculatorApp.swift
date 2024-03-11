09bdbe36f6a75e4ed81aedefa9fe8b3af09f1c2c
Justin Lowry <8211769+Justikun@users.noreply.github.com>
2024-03-11 00:20:20 -0600

    first commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Justin Lowry on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
